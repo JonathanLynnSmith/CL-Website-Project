064eaed66c991f325f969f98ba6f3fb683abd31f
Jonathan Smith <JonathanLynnSmith@gmail.com>
2017-07-16 16:03:40 -0400

    corrected Weather App URL from HTTP to HTTPS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /********************************************************** 
 Weather Application at top of page
 ***********************************************************/
-var weatherAPI = "http://api.wunderground.com/api/2e843102da35bfb9/conditions/q/40047.json";
+var weatherAPI = "https://api.wunderground.com/api/2e843102da35bfb9/conditions/q/40047.json";
 var weatherMethod = function(data) {
     console.log(data);
     var currentTemp = '<h1> ' + data.current_observation.temp_f + 'F </h1>';
