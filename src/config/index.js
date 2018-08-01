46534f1ec0bed9b957d77595b1b3c3b14f1c21c5
Jonathan Smith <Jonathanlynnsmith@gmail.com>
2018-08-01 19:57:25 -0400

    Set Max-width for site

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  appName: 'Our Glorious Node Project',
-  port: 3030,
+  appName: 'Wilchar Estates Website',
+  port: process.env.PORT || 3030,
   db: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
